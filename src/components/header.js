987884e7c6dfde37e47df13c04b5a70720824c63
Leo Torres <leo.htor@gmail.com>
2018-08-25 18:11:03 -0400

    Added animation for header component. Added missing logo to Header.

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
 import Img from 'gatsby-image'
 
 import Logo from '../images/logo.svg'
 
+
 const HeaderWrapper = styled.div`
   background: #524763;
   margin-bottom: 1.45rem;
   position: relative;
   overflow: hidden;
-  height: 70vh;
+  height: ${({isHome}) => (isHome ? '70vh' : '20vh')};
   h1 {
     img {
       height: 80px;
@@ -28,16 +30,34 @@ const HeaderContainer = styled.div`
 
 export default class Header extends Component {
   componentDidUpdate = (prevProps, prevState) => {
-    console.log(this.props.location.pathname);
-    
+    const { location } = this.props
+    if(location.pathname !== prevProps.location.pathname ) {
+      if(this.props.location.pathname === '/') {
+        this.wrapper.animate([ {height: '20vh'}, { height: '70vh' } ], {
+          duration: 300,
+          fill: 'forwards',
+          easing: 'cubic-bezier(0.86, 0, 0.07, 1)',
+          iterations: 1
+        })
+      } else {
+        this.wrapper.animate([{ height: '70vh' }, {height: '20vh'} ], {
+           duration: 300,
+           fill: 'forwards',
+           easing: 'cubic-bezier(0.86, 0, 0.07, 1)',
+           iterations: 1
+         })
+      }
+    }
   }
   
   render() {
-    const { data } = this.props
+    const { data, location } = this.props
     return (
       <div>
-        
-        <HeaderWrapper>
+        <HeaderWrapper 
+          isHome={location.pathname === '/'}
+          ref={(wrapper) => this.wrapper = ReactDOM.findDOMNode(wrapper)}
+        >
           <Img 
             style={{
               position: 'absolute',
@@ -51,7 +71,7 @@ export default class Header extends Component {
           <HeaderContainer>
             <h1 style={{ margin: 0 }}>
               <Link to="/">
-      
+                <img src={Logo} alt="Level up Tuts logo"/>
               </Link>
             </h1>
             
